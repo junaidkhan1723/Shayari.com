4e86290ebc49b6c902f66692c1f357e655d8ad83
Junaid Khan <pathanjunaid7888@gmail.com>
2025-06-12 01:32:58 +0530

    updated layout of card

--- src/components/ShayariForm.jsx
+++ src/components/ShayariForm.jsx
@@ -18,16 +18,14 @@ export default function ShayariForm({ onPost }) {
 
   return (<>
    <Navbar/>
-    <form className="container row my-3 min-vw-100 d-flex">
-      <div className="col-3"></div>
-      <div className="card col-6 p-4">
+    <form className="container  my-3 min-vw-100 d-flex">
+      <div className="card  p-4">
         <h4>Post a Shayari</h4>
         <input type="text" className="form-control my-2" name="name" placeholder="Your Name"    value={formData.name} onChange={handleChange} required/>
         <input type="text" className="form-control my-2" name="category" placeholder="Category" value={formData.category} onChange={handleChange} required/>
         <textarea className="form-control my-3" name="content" placeholder="Your Shayari"       value={formData.content} onChange={handleChange} required></textarea>
         <button className="btn btn-primary" onClick={handleSubmit}>Post</button>
       </div>
-      <div className="col-3"></div>
 
     </form>
     </>
